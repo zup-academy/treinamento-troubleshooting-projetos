726253f39ca99de1d005896e8e06be80e53451ee
Rafael Ponte <rponte@gmail.com>
2021-10-06 19:26:44 -0300

    first commit;

---
+++ bad-customers/src/main/java/br/com/zup/edu/badcustomers/BadCustomersApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.badcustomers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadCustomersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadCustomersApplication.class, args);
+	}
+
+}
---
+++ bad-customers/src/test/java/br/com/zup/edu/badcustomers/BadCustomersApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.badcustomers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BadCustomersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
