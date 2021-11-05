d78813204e2d0286e001b4f155b479ae0b3aa374
Rafael Ponte <rafael.ponte@zup.com.br>
2021-11-05 10:47:05 -0300

    desabilita classe de testes;

--- pix-key-manager/src/test/java/br/com/zup/edu/pixkeymanager/PixKeyManagerApplicationTests.java
+++ pix-key-manager/src/test/java/br/com/zup/edu/pixkeymanager/PixKeyManagerApplicationTests.java
@@ -2,7 +2,9 @@ package br.com.zup.edu.pixkeymanager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Disabled;
 
+@Disabled
 @SpringBootTest
 class PixKeyManagerApplicationTests {
 
